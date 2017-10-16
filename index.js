2af3de777afc7e1068831016c143bb892b2d9b99
Mateusz Stomski <lehronn@gmail.com>
2017-10-16 10:13:11 +0200

    for fixed ver2

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 var fs = require('fs');
-var list;
 
 fs.readdir ('./', (err, files) => {
   for (var i = 0; i < files.length ; i++ ) {
-    // list = files[i] + '/n';
     fs.appendFile('./dirList.log', files[i] + '\n', function (err) {
     if (err) throw err;
     });
